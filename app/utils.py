47decce06963a53ed1b398573ac9cb0c8e87eb01
AMEEN <alameenkp9068@gmail.com>
2024-05-16 10:01:10 +0400

    update deperecated import for colormap

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,5 @@
 import cv2
-import matplotlib.cm as cm
+import matplotlib
 import numpy as np
 
 
@@ -10,7 +10,7 @@ def resize_image(image_data, original_width=200, new_width=150):
 
 
 def apply_colormap(image_array, colour_map='viridis'):
-    colormap = cm.get_cmap(colour_map)
+    colormap = matplotlib.colormaps[colour_map]
     reshaped_image = image_array.reshape((1, -1))
     colored_image = colormap(reshaped_image / 255.0)
     return (colored_image[:, :, :3] * 255).astype(np.uint8)
